6b6e72442dc0e3a569e0e074078cf7cfb4ab036f
WindzoR <windzr67@gmail.com>
2022-12-13 20:49:01 +0300

    interface DishService

--- domain/src/main/java/model/Dish.java
+++ domain/src/main/java/model/Dish.java
@@ -2,6 +2,9 @@ package model;
 
 import lombok.Data;
 
+/**
+ * cookingTime - время готовки в секундах
+ */
 @Data
 public class Dish {
 
@@ -9,11 +12,14 @@ public class Dish {
 
     private String name;
 
+    private long cookingTime;
+
     private boolean available;
 
-    public static Dish of(String name) {
+    public static Dish of(String name, long cookingTime) {
         Dish dish = new Dish();
         dish.name = name;
+        dish.cookingTime = cookingTime;
         dish.available = true;
         return dish;
     }
