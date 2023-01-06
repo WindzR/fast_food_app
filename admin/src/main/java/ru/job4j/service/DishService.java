5716503d2e8d1edc5c62ded5d4b71a3903251030
WindzR <windzr67@gmail.com>
2023-01-06 13:27:18 +0300

    Синхронный обмен сообщений - iteration 2.0

--- admin/src/main/java/ru/job4j/service/DishService.java
+++ admin/src/main/java/ru/job4j/service/DishService.java
@@ -1,5 +1,6 @@
 package ru.job4j.service;
 
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -7,10 +8,12 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import ru.job4j.dto.DishDTO;
 
+import java.util.List;
+
 public class DishService {
 
     @Value("${api-url}")
-    private String url;
+    private String URL;
 
     private final RestTemplate client;
 
@@ -19,6 +22,7 @@ public class DishService {
     }
 
     public DishDTO addNewDish(DishDTO dish) {
+        String url = URL + "/new-dish";
         System.out.println(url);
         return client.postForEntity(
                 url, dish, DishDTO.class
@@ -26,11 +30,64 @@ public class DishService {
     }
 
     public boolean removeDish(String id) {
+        String url = URL + "/" + id;
+        System.out.println(url);
         return client.exchange(
-                String.format("%s?id=%s", url, id),
+                url,
                 HttpMethod.DELETE,
                 HttpEntity.EMPTY,
                 Void.class
         ).getStatusCode() != HttpStatus.NOT_FOUND;
+    }
+
+    public boolean changeDish(String id, DishDTO dish) {
+        String url = URL + "/" + id;
+        System.out.println(url);
+        return client.exchange(
+                url,
+                HttpMethod.PUT,
+                new HttpEntity<>(dish),
+                DishDTO.class
+        ).getStatusCode() != HttpStatus.NOT_FOUND;
+    }
+
+    public boolean setUnavailableDish(String id) {
+        String url = URL + "unavailable/" + id;
+        System.out.println(url);
+        return client.exchange(
+                url,
+                HttpMethod.PATCH,
+                HttpEntity.EMPTY,
+                DishDTO.class
+        ).getStatusCode() != HttpStatus.NOT_FOUND;
+    }
+
+    public List<DishDTO> getAvailableDishes() {
+        String url = URL + "/get-available";
+        System.out.println(url);
+        return client.exchange(
+                url,
+                HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<DishDTO>>() { }
+        ).getBody();
+    }
+
+    public List<DishDTO> getAllDishes() {
+        String url = URL + "/all";
+        System.out.println(url);
+        return client.exchange(
+                url,
+                HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<DishDTO>>() { }
+        ).getBody();
+    }
+
+    public DishDTO getDishById(String id) {
+        String url = URL + "/" + id;
+        System.out.println(url);
+        return client.getForEntity(
+                url,
+                DishDTO.class
+        ).getBody();
     }
 }
