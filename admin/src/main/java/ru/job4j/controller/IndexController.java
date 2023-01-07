8f04978dd5f20ec5f4c0d937635e3b4ac1eff1ca
WindzR <windzr67@gmail.com>
2023-01-08 00:22:55 +0300

    Эксперименты с Thymeleaf

--- admin/src/main/java/ru/job4j/controller/IndexController.java
+++ admin/src/main/java/ru/job4j/controller/IndexController.java
@@ -1,13 +1,42 @@
 package ru.job4j.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.dto.DishDTO;
+import ru.job4j.repository.MemoryRepository;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/dishes")
 public class IndexController {
 
+    private final MemoryRepository repository;
+
+    public IndexController(final MemoryRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/index")
-    public String getIndex() {
-        return "Hello World!";
+    public String getAll(Model model) {
+        model.addAttribute("dishes", repository.getDishes());
+        return "index";
+    }
+
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "dishes/create";
+    }
+
+    @PostMapping("/create")
+    public String create(HttpServletRequest request) {
+        var name = request.getParameter("name");
+        long cookingTime = Long.parseLong(request.getParameter("cookingTime"));
+        DishDTO dto = DishDTO.of(0, name, cookingTime);
+        repository.saveDish(dto);
+        return "redirect:/index";
     }
 }
