f2f289fcc1271ffd8dae1315613924ee147ebb1f
WindzR <windzr67@gmail.com>
2023-01-15 14:01:25 +0300

    Set UP Liquibase in Dish module

---
+++ dish/db/scripts/002_dml_insert_dishes_table.sql
@@ -0,0 +1,3 @@
+insert into dishes (name, cooking_time, available) values ('King Burger', 300, true);
+insert into dishes (name, cooking_time, available) values ('Salad Ceasar', 240, true);
+insert into dishes (name, cooking_time, available) values ('Pepsi-cola', 60, true);
---
+++ dish/src/main/java/ru/job4j/config/LiquibaseConfig.java
@@ -0,0 +1,26 @@
+package ru.job4j.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfig extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LiquibaseConfig.class);
+    }
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:db/liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
+    }
+
+}
